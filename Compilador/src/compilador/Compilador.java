4c93f2feff33b693b9ba5cb53f735c8100c1883d
brivfean <128435123+brivfean@users.noreply.github.com>
2023-03-25 14:33:39 -0600

    Lectura de archivos
    Se agrego la dirección para lectura de archivos, se arreglaron errores y se agregaron comentarios.

--- Compilador/src/compilador/Compilador.java
+++ Compilador/src/compilador/Compilador.java
@@ -9,6 +9,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 import java.util.Scanner;
 
 /**
@@ -20,10 +21,10 @@ public class Compilador {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) throws FileNotFoundException, IOException {
         
     boolean flag = true; //Bandera para validar archivo introducido y parametros con clase validar
-        String doc = ""; //Nombre del documento
+        String doc = "", lect = "" ; //Nombre del documento y lectura del documento
         System.out.println("Introdusca el nombre del archivo con formato: [NOMBRE].TXT");
         Scanner sc = new Scanner(System.in);
         doc = sc.next(); //Recopilar nombre del documento desde la consola
@@ -34,9 +35,12 @@ public class Compilador {
             System.out.println("Introdusca un archivo valido");
         }else{
             System.out.println("Buscando archivo...");
-            File file = new File(doc); //Iniciar el archivo
+            File file = new File("src/Compilador/Lectura/"+doc); //Iniciar el archivo
             FileReader fr = new FileReader(file); //Leer el archivo
-            BufferedReader br = new BufferedReader(fr);
+            BufferedReader br = new BufferedReader(fr); //Buffer
+            while ((lect = br.readLine()) != null) //Extraccion de texto
+            System.out.println(lect);//Imprecion de texto en consola
+            
             
         }
     }
